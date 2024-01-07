39acf97f384368dbb0614e7699a77f946bc00773
Jordan Braz <jordanbraz@outlook.com>
2024-01-07 12:39:21 -0300

    add texture buffer

---
+++ webgpu-vite-vanilla-game/src/geometry.ts
@@ -0,0 +1,32 @@
+export class QuadGeometry {
+    public positions: number[];
+    public colors: number[];
+    public textCoords: number[];
+
+    constructor() {
+        this.positions = [
+            -0.5, -0.5,
+            0.5, -0.5,
+            -0.5, 0.5,
+            -0.5, 0.5,
+            0.5, 0.5,
+            0.5, -0.5,
+        ];
+        this.colors = [
+            1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+        ];
+        this.textCoords = [
+            0.0, 1.0,
+            1.0, 1.0,
+            0.0, 0.0,
+            0.0, 0.0,
+            1.0, 0.0,
+            1.0, 1.0,
+        ]
+    }
+}
